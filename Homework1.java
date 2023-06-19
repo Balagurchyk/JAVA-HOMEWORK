5420b0b4e766f2dffdb9c3a18948ceeebbdf96f0
Balagurchyk <135262924+Balagurchyk@users.noreply.github.com>
2023-06-19 23:35:27 +0200

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,51 @@
+package Homework;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("/'\"работу выполнил Виктор Балагурчик\"'\\");
+        byte myb = 42;
+        short mys = 3457;
+        int myi = 34785;
+        long myl = 52475;
+        float myf = 8.5F;
+        double myd = 42.168;
+        boolean mybool = false;
+        char myc = 'a';
+        System.out.println(myb);
+        System.out.println(mys);
+        System.out.println(myi);
+        System.out.println(myl);
+        System.out.println(myf);
+        System.out.println(myd);
+        System.out.println(mybool);
+        System.out.println(myc);
+        System.out.println("\n");
+
+        myb = 63;
+        mys = 743;
+        myi = 755124;
+        myl = 87412;
+        myf = 93.4F;
+        myd = 745.6247;
+        mybool = true;
+        myc = 'c';
+
+        System.out.println(myb);
+        System.out.println(mys);
+        System.out.println(myi);
+        System.out.println(myl);
+        System.out.println(myf);
+        System.out.println(myd);
+        System.out.println(mybool);
+        System.out.println(myc);
+
+
+
+
+
+
+    }
+
+
+
+}
